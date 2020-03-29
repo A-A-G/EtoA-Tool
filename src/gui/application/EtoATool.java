86351d13f6ec3f5bc34fd4f2ae5dce09a6aca622
Alex <alexander@artigagonzalez.de>
2020-03-29 01:59:43 +0100

    0.5.0

--- src/gui/application/EtoATool.java
+++ src/gui/application/EtoATool.java
@@ -41,7 +41,7 @@ import javafx.stage.Stage;
  */
 public class EtoATool extends Application
 {
-  private final static String VERSION = "0.4.4";
+  private final static String VERSION = "0.5.0";
 
   private final static LocalDate EXPIRE_DATE = LocalDate.of(2020, 05, 01);
 
